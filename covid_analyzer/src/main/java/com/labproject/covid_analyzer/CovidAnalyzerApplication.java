2e7dd4e907a066be0599cbf13ef042b4c1d77c21
Diogo <diogo.fernandes77@ua.pt>
2021-04-01 16:02:34 +0100

    project dir + log4j

---
+++ covid_analyzer/src/main/java/com/labproject/covid_analyzer/CovidAnalyzerApplication.java
@@ -0,0 +1,13 @@
+	package com.labproject.covid_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidAnalyzerApplication.class, args);
+	}
+
+}
---
+++ covid_analyzer/src/test/java/com/labproject/covid_analyzer/CovidAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labproject.covid_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
